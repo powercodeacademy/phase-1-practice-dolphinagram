f885c7a92f0e19d2e85a9cbbd2d33c8f18c1e7e7
smkopp92 <smkopp92@gmail.com>
2021-06-09 11:52:00 -0400

    2nd deliverable

--- src/index.js
+++ src/index.js
@@ -18,19 +18,49 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const dolphinUrl = "http://localhost:3000/dolphins/1"
 
-  fetch(dolphinUrl)
+  // first deliverable code
+  fetch(dolphinUrl) // grab data from server
     .then(response => response.json())
     .then(data => {
-      dolphinName.innerHTML = data.name
+      dolphinName.innerHTML = data.name // use data to update html elements
       dolphinImage.src = data.image
       dolphinLikes.innerText = `${data.likes} likes`
       dolphinComments.innerHTML = ""
-      data.comments.forEach(comment => {
+      data.comments.forEach(comment => { // iterated over comments to add each one
         const listItem = document.createElement("li")
         listItem.innerHTML = comment.content
         dolphinComments.append(listItem)
       })
     })
+
+  // second deliverable code
+  const likeButton = document.querySelector("button.like-button")
+  // const dolphinLikes = document.querySelector("span.likes") this was defined for the first deliverable but we're using it again for the 2nd
+
+  likeButton.addEventListener("click", () => {
+    let numberOfLikes = parseInt(dolphinLikes.innerText)
+    numberOfLikes++
+
+    const formData = {
+      likes: numberOfLikes
+    }
+
+    const configurationObject = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    }
+
+    // const dolphinUrl = "http://localhost:3000/dolphins/1" this was defined for the first deliverable but we're using it again for the 2nd
+    fetch(dolphinUrl, configurationObject)
+      .then(response => response.json())
+      .then(data => {
+        dolphinLikes.innerText = `${data.likes} likes` // brought over from 1st deliverable
+      })
+  })
 })
 
 /*
@@ -46,6 +76,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
 2nd Deliverable
 - Click on the heart icon to increase the dolphin image's likes, and still see them when I reload the page
+
+- Set an event listener to listen to a CLICK on the like BUTTON (button with class of "like-button")
+- After the click occurs:
+  - Find how many likes are currently displayed on the page (span tag with the class of "likes")
+  - Calculate new number of likes (current likes + 1)
+  - Fetch(PATCH) to tell the server what the new number of likes is going to be
+  - Return new value of likes to the page and display it for our awesome users
 
 3rd Deliverable
 - Add a comment (no persistance needed)
